529ce3a75f7a0e6b12e1dbff8e6f1f506b1e57c6
Ann <31072029+annufka@users.noreply.github.com>
2018-12-26 01:24:47 +0200

    Add files via upload

--- server1.py
+++ server1.py
@@ -1,5 +1,25 @@
 import socket
 import time
+import multiprocessing as mp
+
+#процесс
+def one_client(sock):
+    conn, addr = sock.accept()
+    print("connected: ", addr)
+    sock.send(b"OK")
+    class_memory = Memory()
+    while True:
+        data = conn.recv(1024)
+        time_of_message = time.strftime("%Y-%m-%d %H:%M")
+        updata = data.decode("UTF-8")
+        if updata == "all messages":
+            result = list(class_memory.return_list())
+            print(result)
+            conn.send(bytes("{}".format(result), "UTF-8"))
+        else:
+            class_memory.add(time_of_message, updata)
+            print("We added {} and '{}' at the memory".format(time_of_message, updata))
+
 
 #память сервера
 class Memory:
@@ -15,31 +35,13 @@ class Memory:
             yield(key, self.memory[key])
 
 
-clients = {}
-sock = socket.socket()
-sock.setblocking(0)
-sock.bind(('', 9090))
-sock.listen(20)
+with socket.socket() as sock:
+    sock.bind(('', 9090))
+    sock.listen(5)
 
-while True:
-    conn, addr = sock.accept()
-    clients[addr] = conn
-    print("connected: ", addr)
-    class_memory = Memory()
     while True:
-        data = conn.recv(1024)
-        """
-        if not data: 
-            break
-        """
-        time_of_message = time.strftime("%Y-%m-%d %H:%M")
-        updata = data.decode("UTF-8")
-        if updata == "all messages":
-            result = list(class_memory.return_list())
-            print(result)
-            conn.send(bytes("{}".format(result), "UTF-8"))
-        else:
-            class_memory.add(time_of_message, updata)
-            print("We added {} and '{}' at the memory".format(time_of_message, updata))
+        clients = workers = [mp.Process(target=one_client, args=(socket,)) for i in range(5)]
+        for number_of_process in clients:
+            number_of_process.start()
+            number_of_process.join()
 
-sock.close()
