2a79a4c6ed4b6756a8d4b4599f03c670e0a1d18f
Ann <31072029+annufka@users.noreply.github.com>
2019-01-04 15:29:25 +0200

    Create server_tk.py

---
+++ server_tk.py
@@ -0,0 +1,64 @@
+import socket
+import threading
+import time
+
+
+class ClientThread(threading.Thread):
+    # инициализация
+    def __init__(self, client_ip, client_port):
+        threading.Thread.__init__(self)
+        self.client_ip = client_ip
+        self.client_port = client_port
+        print(bytes("New server socket thread started for {}: {}".format(self.client_ip, self.client_port), "utf-8"))
+
+
+    def run(self):
+        self.client_ip.send(bytes("Type your name and press enter", "utf-8"))
+        self.name = client_ip.recv(size).decode("utf-8")
+        self.client_ip.send(bytes("Welcome {} to chat".format(self.name), "utf-8"))
+        message = "{} has joined to chat"
+        self.broadcast(bytes(message.format(self.name), "utf-8"))
+        clients[self.client_ip] = self.name
+        while True:
+            try:
+                data = self.client_ip.recv(size).decode("UTF-8")
+                # получим время сообщения
+                time_of_message = time.strftime("%Y-%m-%d %H:%M:%S")
+                # показать сообщения, которые были добавлены в память
+                if data == "exit":
+                    self.client_ip.send(bytes("Buy {}!".format(self.name), "utf-8"))
+                    self.client_ip.close()
+                    del clients[self.client_ip]
+                    message = "{} has left the chat."
+                    self.broadcast(bytes(message.format(self.name), "utf-8"))
+                    break
+                else:
+                    self.broadcast(data, self.name + ": ")
+            except:
+                pass
+            
+    def broadcast(self, message, prefix=""):
+        for sock_client in clients:
+            sock_client.send(bytes("{} {}".format(prefix, message), "utf-8"))
+
+
+clients = {}
+
+
+ip = 'localhost'
+port = 9090
+size = 1024
+connection = 5
+
+with socket.socket() as sock:
+    sock.bind((ip, port))
+    threads = []
+
+    while True:
+        sock.listen(connection)
+        client_ip, client_port = sock.accept()
+        newthread = ClientThread(client_ip, client_port)
+        newthread.start()
+        threads.append(newthread)
+    for t in threads:
+        t.join()
