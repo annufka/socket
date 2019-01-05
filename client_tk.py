cde515a6a252798d3049510be6028b2458e9d712
Ann <31072029+annufka@users.noreply.github.com>
2019-01-05 10:31:53 +0200

    Update client_tk.py

--- client_tk.py
+++ client_tk.py
@@ -6,7 +6,7 @@ import tkinter
 def receive():
     while True:
         try:
-            msg = client_socket.recv(size).decode("utf8")
+            msg = client_socket.recv(size).decode("utf-8")
             msg_list.insert(tkinter.END, msg)
         except OSError:
             break
@@ -15,22 +15,22 @@ def receive():
 def send(event=None):
     msg = my_msg.get()
     my_msg.set("")
-    client_socket.send(bytes(msg, "utf8"))
-    if msg == "quit":
+    client_socket.send(bytes(msg, "utf-8"))
+    if msg == "exit":
         client_socket.close()
         top.quit()
 
 
 def on_closing(event=None):
-    my_msg.set("quit")
+    my_msg.set("exit")
     send()
 
 top = tkinter.Tk()
-top.title("Chatter")
+top.title("My test chat")
 
 messages_frame = tkinter.Frame(top)
 my_msg = tkinter.StringVar()
-my_msg.set("Type your messages here.")
+my_msg.set("Your message")
 scrollbar = tkinter.Scrollbar(messages_frame)
 msg_list = tkinter.Listbox(messages_frame, height=15, width=50, yscrollcommand=scrollbar.set)
 scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
@@ -45,14 +45,16 @@ send_button = tkinter.Button(top, text="Send", command=send)
 send_button.pack()
 
 top.protocol("WM_DELETE_WINDOW", on_closing)
-
+"""
 HOST = input('Enter host: ')
 PORT = input('Enter port: ')
 if not PORT:
     PORT = 9090
 else:
     PORT = int(PORT)
-
+"""
+HOST = 'localhost'
+PORT = 9090
 size = 1024
 ADDR = (HOST, PORT)
 
